533a2b9b1297817b8a49f05db65d576f8616a1a5
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-02-11 13:59:56 +0100

    Add unit tests

---
+++ Tests.MatecatFilters/ValidatorTests.cs
@@ -0,0 +1,30 @@
+using Apps.MatecatFilters.Connections;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Tests.MatecatFilters.Base;
+
+namespace Tests.MatecatFilters;
+
+[TestClass]
+public class ValidatorTests : TestBase
+{
+    [TestMethod]
+    public async Task ValidatesCorrectConnection()
+    {
+        var validator = new ConnectionValidator();
+
+        var result = await validator.ValidateConnection(Creds, CancellationToken.None);
+        Console.WriteLine(result.Message);
+        Assert.IsTrue(result.IsValid);
+    }
+
+    [TestMethod]
+    public async Task DoesNotValidateIncorrectConnection()
+    {
+        var validator = new ConnectionValidator();
+
+        var newCreds = Creds.Select(x => new AuthenticationCredentialsProvider(x.KeyName, x.Value + "_incorrect"));
+        var result = await validator.ValidateConnection(newCreds, CancellationToken.None);
+        Console.WriteLine(result.Message);
+        Assert.IsFalse(result.IsValid);
+    }
+}
