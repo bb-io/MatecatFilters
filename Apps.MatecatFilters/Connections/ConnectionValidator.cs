1a612dc22e196daae888b579d715fc7a1d5999eb
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-02-11 13:58:26 +0100

    Fix ConnectionValidator

--- Apps.MatecatFilters/Connections/ConnectionValidator.cs
+++ Apps.MatecatFilters/Connections/ConnectionValidator.cs
@@ -1,5 +1,10 @@
-﻿using Blackbird.Applications.Sdk.Common.Authentication;
+﻿using Apps.MatecatFilters.Api;
+using Apps.MatecatFilters.Invocables;
+using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
+using Blackbird.Applications.Sdk.Utils.Extensions.Http;
+using Blackbird.Applications.Sdk.Utils.RestSharp;
+using RestSharp;
 
 namespace Apps.MatecatFilters.Connections;
 
@@ -9,9 +14,37 @@ public class ConnectionValidator: IConnectionValidator
         IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
         CancellationToken cancellationToken)
     {
-        return new()
+        try
         {
-            IsValid = true
-        };
+            var request = new FiltersRequest("/api/v2/original2xliff", Method.Post, authenticationCredentialsProviders).WithFormData(new
+            {
+                sourceLocale = "en-GB",
+                targetLocale = "it-IT",
+            }, isMultipartFormData: true).WithFile(new byte[1], "test.txt", "document");
+
+            var client = new FiltersClient();
+            RestResponse restResponse = await client.ExecuteAsync(request);
+
+            if (!restResponse.IsSuccessStatusCode)
+            {
+                return new ConnectionValidationResponse
+                {
+                    IsValid = false,
+                    Message = restResponse?.ErrorMessage
+                };
+            }
+            return new ConnectionValidationResponse
+            {
+                IsValid = true
+            };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
     }
 }
